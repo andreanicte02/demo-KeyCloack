ba0b75e599bd0d4991fb555373ed979bd60086b4
andreanicte02 <andreanicte02@gmail.com>
2021-03-17 00:01:02 -0600

    -backend

--- cliente1/keycloack-backend/src/main/java/com/demo/keycloackbackend/config/SecurityConfig.java
+++ cliente1/keycloack-backend/src/main/java/com/demo/keycloackbackend/config/SecurityConfig.java
@@ -1,5 +1,55 @@
 package com.demo.keycloackbackend.config;
 
-public class SecurityConfig {
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
+@KeycloakConfiguration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    //estos metodos estan en la documentacion en spring security adapter
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        KeycloakAuthenticationProvider keyProvider = new KeycloakAuthenticationProvider();
+        keyProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keyProvider);
+    }
+
+    //usando tokens
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        super.configure(http);
+        http
+                .authorizeRequests()
+                 .anyRequest().permitAll();
+        http.csrf().disable();
+    }
+
+    @Bean
+    public KeycloakConfigResolver keycloakConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
 }
